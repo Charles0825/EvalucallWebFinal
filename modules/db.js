7679ca655630eabf065457291e15c29000139b6f
Charles Lim <153100685+Charles0825@users.noreply.github.com>
2024-05-04 01:04:59 +0800

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -1,18 +1,18 @@
 const mysql = require("mysql2");
 
-// const connection = mysql.createConnection({
-//   host: "114.29.238.22",
-//   user: "semperxyz",
-//   password: "semperxyz",
-//   database: "evalucall-database",
-// });
+ const connection = mysql.createConnection({
+   host: "114.29.238.22",
+   user: "semperxyz",
+   password: "semperxyz",
+   database: "evalucall-database",
+ });
 
-const connection = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  password: "",
-  database: "evalucall-database",
-});
+//const connection = mysql.createConnection({
+//  host: "127.0.0.1",
+//  user: "root",
+//  password: "",
+//  database: "evalucall-database",
+//});
 
 function executeQuery(query, values, callback) {
   connection.query(query, values, (error, results) => {
